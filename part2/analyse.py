72160e0975772c3d590eab84f60655b0865ed029
Vincent Lemesle <vincent.lemesle@epitech.eu>
2022-12-30 13:38:40 +0100

    edit: part2

--- part2/analyse.py
+++ part2/analyse.py
@@ -1,24 +1,31 @@
 from csv import reader
+from graphviz import Graph
+
 import math
 import numpy as np
-from graphviz import Graph
-import matplotlib.pyplot as plt
 
 
 def compute_dissimilarity(f, s):
-    if int(f[0]) == int(s[0]):
-        dissimilarity_meal = 0
+    if f[3] == s[3]:
+        dissimilarity_job = 0
     else:
-        dissimilarity_meal = 15
-    dissimilarity = math.sqrt(
+        dissimilarity_job = 10
+    if f[4] == s[4]:
+        dissimilarity_city = 0
+    else:
+        dissimilarity_city = 10
+    if f[5] == s[5]:
+        dissimilarity_music = 0
+    else:
+        dissimilarity_music = 10
+    d = math.sqrt(
         (float(f[1]) - float(s[1])) ** 2
-        + 3 * (float(f[2]) - float(s[2])) ** 2
-        + dissimilarity_meal
+        + (float(f[2]) - float(s[2])) ** 2
+        + dissimilarity_job
+        + dissimilarity_city
+        + dissimilarity_music
     )
-    print(
-        f"plyr 1 {int(f[0])}, plyr 2 {int(s[0])}, dissimilarity: {dissimilarity}"
-    )
-    return dissimilarity
+    return d
 
 
 with open('dataset.csv', 'r') as read_obj:
@@ -27,34 +34,34 @@ with open('dataset.csv', 'r') as read_obj:
     for row in csv_reader:
         data.append(row)
     data = data[1:]
-    f = data[0]
-    s = data[1]
     nb_person = len(data)
-    dissimilarity_matrix = np.zeros((nb_person, nb_person))
+    d_matrix = np.zeros((nb_person, nb_person))
     print("compute dissimilarities")
     for person_1_id in range(nb_person):
         for person_2_id in range(nb_person):
-            dissimilarity = compute_dissimilarity(data[person_1_id], data[person_2_id])
-            dissimilarity_matrix[person_1_id, person_2_id] = dissimilarity
-    print(dissimilarity_matrix)
-    threshold = 15
+            dis = compute_dissimilarity(data[person_1_id], data[person_2_id])
+            d_matrix[person_1_id, person_2_id] = dis
+    print('saving dissimilarity matrix...')
+    np.save('dissimilarity_matrix', d_matrix)
+    threshold = 20
+    for d in d_matrix:
+        print(d)
     # build a graph from the dissimilarity
     dot = Graph(comment="Graph created from complex data", strict=True)
     for person_id in range(nb_person):
         dot.node(str(person_id))
     for person_1_id in range(nb_person):
         for person_2_id in range(nb_person):
-            # no self loops
             if not person_1_id == person_2_id:
-                if dissimilarity_matrix[person_1_id, person_2_id] > threshold:
+                if d_matrix[person_1_id, person_2_id] > threshold:
                     dot.edge(
                         str(person_1_id),
                         str(person_2_id),
-                        color="darkolivegreen4",
+                        color="blue",
                         penwidth="1.1",
                     )
-
     # visualize the graph
+    print('preparing the visualisation...')
     dot.attr(label=f"threshold {threshold}", fontsize="20")
-    graph_name = f"images/data_threshold_{threshold}"
+    graph_name = f"result/dissimilarity_{threshold}"
     dot.render(graph_name)
